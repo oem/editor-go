e16938ad57da3c37eabd989e88358a80750e282a
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-03-11 22:04:39 +0100

    makes the test more readable

--- piecetable/piecetable_test.go
+++ piecetable/piecetable_test.go
@@ -3,8 +3,9 @@ package piecetable
 import "testing"
 
 func TestNew(t *testing.T) {
-	new := New("moo")
-	if new.original != "moo" {
-		t.Errorf("Expected new piecetable to have the original buffer set to moo, got %v", new.original)
+	expected := "moo"
+	got := New(expected)
+	if got.original != expected {
+		t.Errorf("Expected new piecetable to have the original buffer set to moo, got %v", got.original)
 	}
 }
