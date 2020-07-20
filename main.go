aa12332e52c09d86808e4c632b22837a4bdf5c09
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-20 21:04:40 +0200

    that's not what this is called anymore

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("replay: a minimal modal editor")
+	fmt.Println("editor-go: tools for text editing")
 	pt := piecetable.New("an example string")
 	pt.Insert("unremarkable ", 3)
 	fmt.Println(pt, pt.Get())
